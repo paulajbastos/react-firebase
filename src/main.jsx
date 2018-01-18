41ad4b40e34d9d9dc560e2a0bc564efc6b79fe86
Paula Junqueira Bastos <paulajbastos@gmail.com>
2018-01-18 13:05:09 -0200

    autenticacao de usuarios

--- src/main.jsx
+++ src/main.jsx
@@ -29,43 +29,121 @@ class Main extends Component {
 
   }
 
-  listData() {
+  createUser() {
+    var email = "paulajbastos@gmail.com";
+    var senha = "admin123";
 
-    var marcas = firebase.database().ref("marcas");
+    const user = firebase.auth();
 
-    //Adicionando listener -> https://firebase.google.com/docs/database/web/lists-of-data
-    marcas.on('value', (snapshot) => {
-      alert(snapshot.val());
-    });
+    user.createUserWithEmailAndPassword(
+      email,
+      senha
+    ).catch(
+      (error) => {
+        //error.code, error.message
+        var msgErro = '';
+        if(error.code == 'auth/weak-password') {
+          msgErro = 'A senha precisa ter no mínimo 6 caracteres';
+        }
+        alert(msgErro);
+      }
+    )
+  }
 
+  verifyUser() {
+
+    const user = firebase.auth();
+
+    //Criando um listener
+    user.onAuthStateChanged(
+      (userLogged) => {
+        if(userLogged) {
+          alert('Usuário está logado');
+        }else {
+          alert('Usuário não está logado');
+        }
+      }
+    );
+
+    // const userLogged = user.currentUser;
+    // if(userLogged) {
+    //   alert('Usuário está logado');
+    // }else {
+    //   alert('Usuário não está logado');
+    // }
 
   }
 
+  logOffUser() {
 
-  saveData() {
-    var marcas = firebase.database().ref("marcas");
+    const user = firebase.auth();
+    user.signOut();
 
-    //Gera estaticamente o nó
-    //marcas.child('001').child('nome').set('Liquidação');
+  }
 
-    //Gera dinamicamente um identificador unico para cada nó
-    //marcas.push().child('nome').set('Liquidação');
+  logInUser() {
 
-    //Criando com objetos literais
+    //https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#signInWithEmailAndPassword
 
-    marcas.push().set(
-      {
-        nome: 'Liquidaçao',
-        url: 'https://www.usereserva.com/usereserva/home',
-        title: 'Liquidaçao',
-        marca: 'reserva',
-        categoryid: '254898A1278946E325F84904BC6922C8'
+    var email = "paulajbastos@gmail.com";
+    var senha = "admin123";
+
+    const user = firebase.auth();
+
+    user.signInWithEmailAndPassword(
+      email,
+      senha
+    ).catch(
+      (error) => {
+        //error.code, error.message
+         alert(error.message);
+        // var msgErro = '';
+        // if(error.code == 'auth/weak-password') {
+        //   msgErro = 'A senha precisa ter no mínimo 6 caracteres';
+        // }
+        // alert(msgErro);
       }
     )
 
-
-
   }
+
+  // listData() {
+
+  //   var marcas = firebase.database().ref("marcas");
+
+  //   //Adicionando listener -> https://firebase.google.com/docs/database/web/lists-of-data
+  //   marcas.on('value', (snapshot) => {
+  //     alert(snapshot.val());
+  //   });
+
+
+  // }
+
+
+  // saveData() {
+  //   var marcas = firebase.database().ref("marcas");
+
+  //   //Gera estaticamente o nó
+  //   //marcas.child('001').child('nome').set('Liquidação');
+
+  //   //Gera dinamicamente um identificador unico para cada nó
+  //   //marcas.push().child('nome').set('Liquidação');
+
+  //   //Criando com objetos literais
+
+  //   marcas.push().set(
+  //     {
+  //       nome: 'Liquidaçao',
+  //       url: 'https://www.usereserva.com/usereserva/home',
+  //       title: 'Liquidaçao',
+  //       marca: 'reserva',
+  //       categoryid: '254898A1278946E325F84904BC6922C8'
+  //     }
+  //   )
+
+
+
+  // }
 
   render() {
     return (
@@ -81,13 +159,22 @@ class Main extends Component {
 
                 <input type="text" name="username" placeholder="What's your name?" />
                 <input type="text" name="currentItem" placeholder="What are you bringing?" />
-                <button
-                onClick={()=> this.saveData()}
-                >Add Item</button>
 
                 <button
-                onClick={()=> this.listData()}
-                >Mostrar</button>
+                onClick={()=> this.createUser()}
+                >Cadastrar Usuario</button>
+
+                <button
+                onClick={()=> this.verifyUser()}
+                >Verificar Usuario Logado</button>
+
+                <button
+                onClick={()=> this.logOffUser()}
+                >Deslogar Usuario</button>
+
+                <button
+                onClick={()=> this.logInUser()}
+                >Logar Usuario</button>
 
           </section>
           <section className='display-item'>
